53697adaceae5441d4d8715e5a111e1561184837
Tamara Trotter <tltrotte@me.com>
2017-10-20 09:24:32 -0400

    Final Commit

--- grant.js
+++ grant.js
@@ -36,21 +36,21 @@ while(grantpoints >= 0 && namepoints >= 0){
   namepoints -= battle2;
   console.log(name +" has " + namepoints + " points left.");
   console.log("Almightly Grant has " + grantpoints + " points left.");
-  if (grantpoints === 0) {
+  if (grantpoints <= 0) {
     grantLosses++;}
 
-    if (grantpoints === 0 && grantLosses < 3) {
-		grantpoints = 10;
-		}
-}
-	if (namepoints = 0) {
-	grantWins ++;
-	console.log(name +" has lost the game.");
-}
-else { nameWins++;
-	  console.log (name + " wins the game!");
+    if (grantpoints <= 0 && grantLosses < 3) {
+      grantpoints = 10;
+    }
+  }
+  if (namepoints <= 0) {
+    grantWins ++;
+    console.log(name +" has lost the game.");
+  }
+  else { nameWins++;
+    console.log (name + " wins the game!");
 
-	}
+  }
   console.log ("FINAL SCORE")
   console.log(grantWins + " wins for Almighty Grant");
   console.log(+ nameWins + " wins for " + name );
