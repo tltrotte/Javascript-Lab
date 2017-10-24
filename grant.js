5cc2a0f0042c5b820be2db00c2a446fb123d0eb1
Tamara Trotter <tltrotte@me.com>
2017-10-24 07:48:47 -0400

    Lab part 2 comitt

--- grant.js
+++ grant.js
@@ -1,5 +1,5 @@
 
-var start = prompt ("Would you like to play a game?");
+/*var start = prompt ("Would you like to play a game?");
 
 
 if(start==='yes'){
@@ -19,19 +19,9 @@ var round = 2
 // game battle
 
 
-// for (name=40; name>0; name=Math.floor(Math.random() * 2) + 1 {
-//  console.log(name);
-
-
-
-
-//var battle = Math.floor(Math.random(2+1) * 2) + 1;
-//var i = (grantpoints && userpoints);
-//var a = (grantpoints || userpoints);
-
 while(grantpoints >= 0 && namepoints >= 0){
-  var battle = Math.floor(Math.random(2+1) * 2) + 1;
-  var battle2 = Math.floor(Math.random(2+1) * 2) + 1;
+  var battle = Math.floor(Math.random() * 2) + 1;
+  var battle2 = Math.floor(Math.random() * 2) + 1;
   grantpoints -= battle;
   namepoints -= battle2;
   console.log(name +" has " + namepoints + " points left.");
@@ -54,4 +44,59 @@ while(grantpoints >= 0 && namepoints >= 0){
   }
   console.log ("FINAL SCORE")
   console.log(grantWins + " wins for Almighty Grant");
+  console.log(+ nameWins + " wins for " + name );*/
+
+  // Lab Part 2
+  function startGame (){
+  var start = prompt ("Would you like to play a game?");
+    if(start==='yes'){
+      var name = prompt ('What do they call you?');
+      startCombat(name);
+  }
+  }
+  startGame();
+
+function startCombat(user){
+  function getDamage(){
+    return Math.floor(Math.random() * 5) + 1
+   }
+var name = user;
+var grantpoints = 10;
+var namepoints = 40;
+var grantLosses = 0;
+var nameWins = 0;
+var grantWins = 0;
+var round = 2;
+
+  while(grantpoints >= 0 && namepoints >= 0){
+  var damage =  prompt("Attack or quit");
+  if(damage === "Attack"){
+      grantpoints -= getDamage();
+      namepoints -= getDamage();
+      console.log(name +" has " + namepoints + " points left.");
+      console.log("Almightly Grant has " + grantpoints + " points left.");
+      if (grantpoints <= 0) {
+        grantLosses++;}
+
+        if (grantpoints <= 0 && grantLosses < 3) {
+          grantpoints = 10;
+          console.log ("Round " + round++ )
+        }
+      if (namepoints <= 0) {
+        grantWins ++;
+        console.log(name +" has lost the game.");
+      }
+      else { nameWins++;
+        console.log (name + " wins the game!");
+
+  } if( damage==="quit"){
+  console.log ("You're lucky!")
+  break;
+}
+}
+  console.log ("FINAL SCORE")
+  console.log(grantWins + " wins for Almighty Grant");
   console.log(+ nameWins + " wins for " + name );
+
+  }
+}
